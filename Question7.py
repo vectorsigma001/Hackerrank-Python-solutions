7eb8ed65ced567322f8818d0682b8153572fdca4
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-08-14 20:54:21 +0545

    Create Question7.py

---
+++ Question7.py
@@ -0,0 +1,13 @@
+"""
+print from 1 to n numbers 
+
+"""
+if __name__ == '__main__':
+    n = int(input())
+    a=""
+    i=1
+    while i<=n:
+        a=a+str(i)
+        i=i+1
+    print(a)
+            
