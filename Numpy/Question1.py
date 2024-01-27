810a86f50b75ca4f804285ccaab6a755d1ea5b80
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-01-27 14:33:09 +0545

    Update Question1.py

--- Numpy/Question1.py
+++ Numpy/Question1.py
@@ -1,20 +1,52 @@
+"""
+You are given two integer array a and b
+and perform differet operations
+
+"""
 import numpy
+
 if __name__ == "__main__":
     n, m = map(int, input().split())
-    a = numpy.array(input().split(), float)
-    b = numpy.array(input().split(), float)
+    a = []
+    for _ in range(n):
+        row = numpy.array(input().split(), float)
+        a.append(row)
+    a = numpy.array(a)
+    
+    b = []
+    for _ in range(n):
+        row = numpy.array(input().split(), float)
+        b.append(row)
+    b = numpy.array(b)
 
-    # here I am performing addition
     summation = numpy.add(a, b).astype(int)
     difference = numpy.subtract(a, b).astype(int)
     multiplication = numpy.multiply(a, b).astype(int)
     division = numpy.divide(a, b).astype(int)
     mod = numpy.mod(a, b).astype(int)
     power = numpy.power(a, b).astype(int)
-    
-    print(summation.reshape(1, -1))
-    print(difference.reshape(1, -1))
-    print(multiplication.reshape(1, -1))
-    print(division.reshape(1, -1))
-    print(mod.reshape(1, -1))
-    print(power.reshape(1, -1))
+
+    print(summation)
+    print(difference)
+    print(multiplication)
+    print(division)
+    print(mod)
+    print(power)
+
+"""
+input
+1 4
+1 2 3 4
+5 6 7 8
+"""
+
+"""
+output
+[[ 6  8 10 12]]
+[[-4 -4 -4 -4]]
+[[ 5 12 21 32]]
+[[0 0 0 0]]
+[[1 2 3 4]]
+[[    1    64  2187 65536]]
+
+"""
