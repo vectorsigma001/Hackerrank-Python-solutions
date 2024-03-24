790e8b06a9055d3aa5102af8019b8f9e343d319f
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-03-24 08:14:44 +0545

    Create zerosandones.py

---
+++ Numpy/zerosandones.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+n = list(map(int, input().split()))
+print(np.zeros((n),int))
+print(np.ones((n),int)) 
+"""
+Sample input
+3 3 3
+
+Sample output
+[[[0 0 0]
+  [0 0 0]
+  [0 0 0]]
+ [[0 0 0]
+  [0 0 0]
+  [0 0 0]]
+ [[0 0 0]
+  [0 0 0]
+  [0 0 0]]]
+[[[1 1 1]
+  [1 1 1]
+  [1 1 1]]
+ [[1 1 1]
+  [1 1 1]
+  [1 1 1]]
+ [[1 1 1]{-truncated-}
+"""
